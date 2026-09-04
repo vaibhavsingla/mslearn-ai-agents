c1f3854b0b3af05b6bc94a865eb47d8e9b43ae62
Vaibhav Singla <vaibhav.simply@gmail.com>
2026-09-04 17:09:52 +0530

    practice

--- Labfiles/03-mcp-integration/Python/client.py
+++ Labfiles/03-mcp-integration/Python/client.py
@@ -8,9 +8,12 @@ from azure.ai.projects.models import FunctionTool
 from azure.identity import DefaultAzureCredential
 from azure.ai.projects.models import PromptAgentDefinition, FunctionTool
 from openai.types.responses.response_input_param import FunctionCallOutput, ResponseInputParam
+from requests import session
 
 # Add references
-
+# from asyncio import tools
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
 
 # Clear the console
 os.system('cls' if os.name=='nt' else 'clear')
@@ -28,13 +31,17 @@ async def connect_to_server(exit_stack: AsyncExitStack):
     )
 
     # Start the MCP server
-
+    stdio_transport = await exit_stack.enter_async_context(stdio_client(server_params))
+    stdio, write = stdio_transport
 
     # Create an MCP client session
-    
+    session = await exit_stack.enter_async_context(ClientSession(stdio, write))
+    await session.initialize()
 
     # List available tools
-   
+    response = await session.list_tools()
+    tools = response.tools
+    print("\nConnected to server with tools:", [tool.name for tool in tools]) 
 
     return session
 
@@ -52,13 +59,45 @@ async def chat_loop(session):
         tools = response.tools
 
         # Build a function for each tool
+        def make_tool_func(tool_name):
+            async def tool_func(**kwargs):
+                result = await session.call_tool(tool_name, kwargs)
+                return result
 
+            tool_func.__name__ = tool_name
+            return tool_func
+        
+        # Store the functions in a dictionary for easy access when processing function calls
+        functions_dict = {tool.name: make_tool_func(tool.name) for tool in tools}
 
         # Create FunctionTool definitions for the agent
-        
+        mcp_function_tools: FunctionTool = []
+        for tool in tools:
+            function_tool = FunctionTool(
+                name=tool.name,
+                description=tool.description,
+                parameters={
+                    "type": "object",
+                    "properties": {},
+                    "additionalProperties": False,
+                },
+                strict=True
+            )
+            mcp_function_tools.append(function_tool)
 
         # Create the agent
-
+        agent = project_client.agents.create_version(
+            agent_name="inventory-agent",
+            definition=PromptAgentDefinition(
+                model=model_deployment,
+                instructions="""
+                You are an inventory assistant. Here are some general guidelines:
+                - Recommend restock if item inventory < 10  and weekly sales > 15
+                - Recommend clearance if item inventory > 20 and weekly sales < 5
+                """,
+                tools=mcp_function_tools
+            ),
+        )
 
         # Create a thread for the chat session
         conversation = openai_client.conversations.create()
@@ -90,10 +129,33 @@ async def chat_loop(session):
                 print(f"Response failed: {response.error}")
 
             # Process function calls
+            for item in response.output:
+                if item.type == "function_call":
+                    # Retrieve the matching function tool
+                    function_name = item.name
+                    kwargs = json.loads(item.arguments)
+                    required_function = functions_dict.get(function_name)
 
+                    # Invoke the function
+                    output = await required_function(**kwargs)
+
+                    # Append the output text
+                    input_list.append(
+                    FunctionCallOutput(
+                        type="function_call_output",
+                        call_id=item.call_id,
+                        output=output.content[0].text,
+                    )
+                )
 
             # Send function call outputs back to the model and retrieve a response
-           
+            if input_list:
+                response = openai_client.responses.create(
+                        input=input_list,
+                        previous_response_id=response.id,
+                        extra_body={"agent_reference": {"name": agent.name, "type": "agent_reference"}},
+                )
+            print(f"Agent response: {response.output_text}")
            
         # Delete the agent when done
         print("Cleaning up agents:")
--- Labfiles/03-mcp-integration/Python/server.py
+++ Labfiles/03-mcp-integration/Python/server.py
@@ -1,11 +1,11 @@
 # Add references
-
+from fastmcp import FastMCP
 
 # Create an MCP server
-
+mcp = FastMCP(name="Inventory")
 
 # Add an inventory check mcp tool
-
+@mcp.tool
 def get_inventory_levels() -> dict:
     """Returns current inventory for all products."""
     return {
@@ -22,7 +22,7 @@ def get_inventory_levels() -> dict:
     }
 
 # Add a weekly sales mcp tool
-
+@mcp.tool
 def get_weekly_sales() -> dict:
     """Returns number of units sold last week."""
     return {
@@ -39,3 +39,4 @@ def get_weekly_sales() -> dict:
     }
 
 # Run the MCP server
+mcp.run(show_banner=False)
